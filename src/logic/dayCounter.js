f55d1d03fc0f5ad4dc53070f6f9fc0ec7b6ec1be
tevan.carrell <tevanc@zillowgroup.com>
2018-10-29 17:07:12 -0500

    1.3.0 - Updated all dates to be moment.js

--- src/logic/dayCounter.js
+++ src/logic/dayCounter.js
@@ -1,16 +1,18 @@
+const moment = require("moment");
+
 export default function daysLeft(date) {
-  const startDate = new Date();
+  const startDate = moment();
   const endDate = date;
   return countDays(startDate, endDate);
 }
 
 function countDays(startDate, endDate) {
   let count = 0;
-  const curDate = startDate;
-  while (curDate <= endDate) {
-    const dayOfWeek = curDate.getDay();
+  let currentDate = startDate;
+  while (currentDate <= endDate) {
+    const dayOfWeek = currentDate.day();
     if (!(dayOfWeek === 6 || dayOfWeek === 0)) count++;
-    curDate.setDate(curDate.getDate() + 1);
+    currentDate = currentDate.add(1, "days");
   }
   return count;
 }
